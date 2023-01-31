8b606a5da5527c4a54f048f3d12f4b489d1326df
DRV111 <xblackcrownx@gmail.com>
2023-01-31 16:47:48 +0300

    feat: add NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import styles from './style';
+import {
+  NavBar,
+  Hero,
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Stats,
+  Testimonials,
+  Footer,
+} from './components';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>App</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
   </div>
 );
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const NavBar = () => {
+  const [isClose, setIsclose] = useState(false);
+  return (
+    <nav className="w-full flex py-6 justify-between item-center navbar">
+      <img src={logo} alt="Logo" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((item, index) => (
+          <li
+            key={item.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            } text-white`}
+          >
+            <a href={`#${item.id}`}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={isClose ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => {
+            setIsclose((prev) => !prev);
+          }}
+        />
+        <div
+          className={`${
+            isClose ? 'flex' : 'hidden'
+          } p-6 bg-black-gradient absolute top-12 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((item, index) => (
+              <li
+                key={item.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
+                } text-white`}
+              >
+                <a href={`#${item.id}`}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
